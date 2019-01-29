b32d669d8cde55d1f81dc5c76b8c8acff27a38ab
Jack Ingham <3inghj71@solent.ac.uk>
2019-01-29 18:12:36 +0000

    Second map task done, needs signed floats added and formatting

--- AAA_MyClassWork/session_3/MapsTest2/app/src/main/java/uk/ac/solent/mapstest1/MainActivity.java
+++ AAA_MyClassWork/session_3/MapsTest2/app/src/main/java/uk/ac/solent/mapstest1/MainActivity.java
@@ -12,21 +12,19 @@ import android.view.View.OnClickListener;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.EditText;
-
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements OnClickListener
 {
 
     MapView mv;
 
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
@@ -34,19 +32,32 @@ public class MainActivity extends AppCompatActivity
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
 
         setContentView(R.layout.activity_main);
-        mv = (MapView)findViewById(R.id.map1);
+        mv = (MapView) findViewById(R.id.map1);
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(16);
-        mv.getController().setCenter(new GeoPoint(50.939652,-0.438075));
-        Button b = (Button)findViewById(R.id.btn1);
+        mv.getController().setCenter(new GeoPoint(50.939652, -0.438075));
+        Button b = (Button) findViewById(R.id.btn1);
         b.setOnClickListener(this);
+    }
 
-        public void onClick(View view)
+    public void onClick(View view)
         {
             mv = (MapView)findViewById(R.id.map1);
             EditText et_lat = (EditText)findViewById(R.id.et1);
-            EditText et_long = (EditText)findViewById(R.id.et1);
+            EditText et_long = (EditText)findViewById(R.id.et2);
+
+            String LatString= et_lat.getText().toString();
+            float latitude=Float.parseFloat(LatString);
+
+            String LongString= et_long.getText().toString();
+            float longitude=Float.parseFloat(LongString);
+
+            mv.setBuiltInZoomControls(true);
+            mv.getController().setZoom(16);
+            mv.getController().setCenter(new GeoPoint(latitude, longitude));
+
+
 
         }
-    }
+
 }
