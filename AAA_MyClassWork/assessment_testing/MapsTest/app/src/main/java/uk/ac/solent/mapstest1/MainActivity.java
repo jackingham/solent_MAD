7d5a185b9323070b4083149a028531e387337724
Jack Ingham <3inghj71@solent.ac.uk>
2019-03-19 17:53:24 +0000

    Added markers from file

--- AAA_MyClassWork/assessment_testing/MapsTest/app/src/main/java/uk/ac/solent/mapstest1/MainActivity.java
+++ AAA_MyClassWork/assessment_testing/MapsTest/app/src/main/java/uk/ac/solent/mapstest1/MainActivity.java
@@ -37,15 +37,23 @@ import android.widget.Toast;
 
 import android.preference.PreferenceManager;
 import android.widget.Toast;
+
 import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.OverlayItem;
+
 import java.util.ArrayList;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import android.os.Environment;
 
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
     MapView mv;
     ItemizedIconOverlay<OverlayItem> items;
-
+    OverlayItem currentLocation = null;
 
     /*** Called when the activity is first created.*/
     @Override
@@ -82,10 +90,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         };
 
         items = new ItemizedIconOverlay<OverlayItem>(this, new ArrayList<OverlayItem>(), markerGestureListener);
-        OverlayItem fernhurst = new OverlayItem("Fernhurst", "Village in West Sussex", new GeoPoint(51.05, -0.72));
-        OverlayItem blackdown = new OverlayItem("Blackdown", "highest point in West Sussex", new GeoPoint(51.0581, -0.6897));
-        items.addItem(fernhurst);
-        items.addItem(blackdown);
         mv.getOverlays().add(items);
 
 
@@ -93,10 +97,13 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     public void onLocationChanged(Location newLoc) {
 
+        if (currentLocation != null) {
+            items.removeItem(currentLocation);
+        }
         Double newLat = newLoc.getLatitude();
         Double newLong = newLoc.getLongitude();
         mv.getController().setCenter(new GeoPoint(newLat, newLong));
-        OverlayItem currentLocation = new OverlayItem("You are here", "", new GeoPoint(newLat, newLong));
+        currentLocation = new OverlayItem("You are here", "", new GeoPoint(newLat, newLong));
         items.addItem(currentLocation);
         currentLocation.setMarker(getResources().getDrawable(R.drawable.marker));
 
@@ -119,9 +126,8 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     }
 
 
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        MenuInflater inflater=getMenuInflater();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
     }
@@ -134,6 +140,31 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             startActivityForResult(intent, 0);
             return true;
 
+        } else if (item.getItemId() == R.id.loadfile) {
+            try {
+                FileReader fr = new FileReader(Environment.getExternalStorageDirectory().getAbsolutePath() + "/restaurants.csv");
+                BufferedReader reader = new BufferedReader(fr);
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] components = line.split(",");
+                    if (components.length == 5) {
+                        String newName = components[0];
+                        String newType = components[1];
+                        String newLoc = components[2];
+                        double newLong = Double.parseDouble(components[3]);
+                        double newLat = Double.parseDouble(components[4]);
+
+                        OverlayItem newItem = new OverlayItem(newName, (newType+" in "+newLoc ), new GeoPoint(newLat,newLong));
+                        items.addItem(newItem);
+                    }
+                }
+            } catch (IOException e) {
+                new AlertDialog.Builder(this).setPositiveButton("OK", null).
+                        setMessage("ERROR: " + e).show();
+
+            }
+
+
         }
         return false;
     }
@@ -145,12 +176,12 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             if (resultCode == RESULT_OK) {
                 Bundle extras = intent.getExtras();
                 String name_in = extras.getString("name_out");
-                String type_in = extras.getString("name_out");
-                String description_in = extras.getString("name_out");
+                String type_in = extras.getString("type_out");
+                String description_in = extras.getString("description_out");
 
-                OverlayItem name = new OverlayItem(name_in, description_in, new GeoPoint(51.0581, -0.6897));
+                OverlayItem name = new OverlayItem(name_in, description_in, new GeoPoint(currentLocation.getPoint().getLatitude(), currentLocation.getPoint().getLongitude()));
                 items.addItem(name);
-
+                Toast.makeText(this, "Marker added at current position.", Toast.LENGTH_LONG).show();
 
 
             }
